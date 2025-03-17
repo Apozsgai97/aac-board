9db901d15657896f9801ae3f33270cb260fd75a3
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-17 09:51:16 +0100

    Refactor button creation in main_board to use a dictionary for command functions

--- main_board.py
+++ main_board.py
@@ -15,11 +15,27 @@ WORDS = [
     ["People", "have", "stop", "to", "Food & Drink"],
     ["Questions", "help", "Actions", "Connecting Words", "Animals"]]
 
+WORD_FUNCTIONS = {
+    "Questions": open_questions,
+    "Time": open_time,
+    "Places": open_places,
+    "Animals": open_animals,
+    "Food & Drink": open_food_drink,
+    "More things": open_more_things,
+    "Descriptors": open_descriptors
+}
+
 
 def create_buttons(root, speak_button, delete_button, back_button, update_input):
     back_button.grid_forget()
     for i, row in enumerate(WORDS):
         for index, word in enumerate(row):
+
+            if word in WORD_FUNCTIONS:
+                command_function = lambda w=word: WORD_FUNCTIONS[w](root, speak_button, delete_button, back_button, update_input)
+            else:
+                command_function = lambda w=word: [update_input(w), speak(w)]
+
             if index == 0 and i == 4:
                 color = "blue"
             elif index == 0:
@@ -30,36 +46,10 @@ def create_buttons(root, speak_button, delete_button, back_button, update_input)
                 color = "purple"
             elif index == 3:
                 color = "orange"
-
-            if word == "Questions":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda: [open_questions(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "Time":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_time(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "Places":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_places(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "Animals":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_animals(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "Food & Drink":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_food_drink(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "More things":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_more_things(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
-            elif word == "Descriptors":
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [open_descriptors(root, speak_button, delete_button, back_button, update_input)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
             else:
-                button = tk.Button(root, text=word, width=20,
-                                   height=4, font=("Arial", 16), highlightbackground=color, bg=color, command=lambda w=word: [update_input(w), speak(w)])
-                button.grid(row=i + 1, column=index, padx=5, pady=5)
+                color = "grey"
+
+            button = tk.Button(root, text=word, width=20,
+                               height=4, font=("Arial", 16), highlightbackground=color, bg=color,  command=command_function
+                               )
+            button.grid(row=i + 1, column=index, padx=5, pady=5)
