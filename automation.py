c5544c2e076c1250773bb2d0459cade2a1489fb0
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-18 11:43:10 +0100

    Add automation module with word sorting and sublist creation functions

---
+++ automation.py
@@ -0,0 +1,34 @@
+test_words = [
+    "Big", "Small", "Fast", "Slow",
+    "Happy", "Sad", "Angry", "Excited", "Tired",
+    "Bright", "Dark", "Noisy", "Quiet", "Calm",
+    "Soft", "Hard", "Hot", "Cold", "Warm",
+    "Friendly", "Brave", "Shy", "Strong", "Weak"
+]
+
+
+
+def sort_words(words):
+    
+    sorted_words = sorted(words)
+    
+    return sorted_words
+
+def make_sublists(words):
+    
+    sublists = []
+
+    sublists.append(words[0:4])
+
+    remaining_words = words[4:]
+    
+    for i in range(0, len(remaining_words), 5):
+        sublists.append(remaining_words[i:i+5])
+    print(sublists)
+    return sublists
+
+
+sort_words(test_words);
+make_sublists(test_words);
+
+
