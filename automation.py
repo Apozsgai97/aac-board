93b71616122a192afa670cb6744e7220665bec6c
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-18 11:46:00 +0100

    Refactor sort_words function to return sublists of sorted words

--- automation.py
+++ automation.py
@@ -7,28 +7,22 @@ test_words = [
 ]
 
 
-
 def sort_words(words):
-    
+
     sorted_words = sorted(words)
-    
-    return sorted_words
+    sublists = make_sublists(sorted_words)
+    return sublists
+
 
 def make_sublists(words):
-    
+
     sublists = []
 
     sublists.append(words[0:4])
 
     remaining_words = words[4:]
-    
+
     for i in range(0, len(remaining_words), 5):
         sublists.append(remaining_words[i:i+5])
-    print(sublists)
+
     return sublists
-
-
-sort_words(test_words);
-make_sublists(test_words);
-
-
