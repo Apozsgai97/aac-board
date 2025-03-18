b33a48d4906dbc3a52490f36ce8fe8d032727ffe
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-03-18 14:33:40 +0100

    Add grammar checking functionality to text_to_speech module

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import requests
 
 def speak(text):
     if text:
@@ -6,3 +7,25 @@ def speak(text):
         engine = pyttsx3.init()
         engine.say(text)
         engine.runAndWait()
+
+
+def check_grammar(text):
+    url = "https://api.languagetool.org/v2/check"
+    playload = {
+        "text": text,
+        "language": "en-US"
+    }
+    response = requests.post(url, data=playload)
+    result = response.json()
+
+    if result["matches"]:
+        corrected_text = text
+        for match in result["matches"]:
+          if match["replacements"]:
+              incorrect_part = match["context"]["text"][match["offset"]:match["offset"] + match["length"]]
+              correction = match["replacements"][0]["value"]
+              corrected_text = corrected_text.replace(
+                  incorrect_part, correction, 1)
+        return corrected_text
+    else:
+        return text
